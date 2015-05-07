7593cf1a8d1be616e326b6473e0ab2e3b0f1cf2f
dkoleary88 <dan@dko.io>
2015-05-08 05:51:32 +1000

    Add nickname function

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+
+// CHAT IO //
 var io = require('socket.io').listen(server);
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 });
 
-// ROUTING: /
+// ROUTING: //
 app.get('/', function (request, response) {
 	response.sendFile(__dirname + '/index.html');
 });
